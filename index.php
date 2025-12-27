835e29abad9c2175c4727a7d92f65038f0512eed
jobaer360s <jobayer360s@gmail.com>
2025-12-27 21:49:57 +0600

    Index , header footer

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-echo " checking index "
+<?php
+
+require 'views/header.php';
+?>
+
+
+<main>
+    <section style="text-align: center; padding: 50px;">
+        <h1>Welcome to AgriSmart</h1>
+        <p>Your smart solution for modern agriculture.</p>
+        
+    </section>
+</main>
+
+<?php
+
+require 'views/footer.php';
 ?>
